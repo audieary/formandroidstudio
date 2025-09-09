eab72090b137663698623e466405fad840a24ff2
audieary <cimolbumbubalado@gmail.com>
2025-09-09 15:00:22 +0700

    pesan commit

--- app/src/main/java/com/example/projekdua/MainActivity.java
+++ app/src/main/java/com/example/projekdua/MainActivity.java
@@ -2,43 +2,43 @@ package com.example.projekdua;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     EditText editText;
-    RadioButton radioButton1;
-    RadioButton radioButton2;
-    RadioButton radioButton3;
+    RadioButton radio1;
+    RadioButton radio2;
+    RadioButton radio3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        editText = findViewById(R.id.editText);
-        radioButton1 = findViewById(R.id.radioButton1);
-        radioButton2 = findViewById(R.id.radioButton2);
-        radioButton3 = findViewById(R.id.radioButton3);
-    }
+        editText=findViewById(R.id.editText);
+        radio1=findViewById(R.id.radioButton1);
+        radio2=findViewById(R.id.radioButton2);
+        radio3=findViewById(R.id.radioButton3);
 
+    }
     public void onCreate(View view) {
-        String phone = editText.getText().toString();
-        String pilih = "";
-        if (radioButton1.isChecked()) {
-            pilih = "telp pribadi";
-        } else if (radioButton2.isChecked()) {
-            pilih = "telp kantor";
-        } else if (radioButton3.isChecked()) {
-            pilih = "telp rumah";
+        String nophone = editText.getText().toString();
+        String pilih = " ";
+        if (radio1.isChecked()) {
+            pilih = "Telp Rumah";
+        } else if (radio2.isChecked()) {
+            pilih = "Mobile";
+        } else if (radio3.isChecked()) {
+            pilih = "Telp Kantor";
         }
-        Toast.makeText(this, pilih + " " + phone, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, pilih + " " + nophone, Toast.LENGTH_SHORT).show();
     }
 }
